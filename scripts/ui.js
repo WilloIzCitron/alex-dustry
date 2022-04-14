19b00ec2179a13fc37609829a652ba2ac2eb1ec9
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2022-04-15 06:07:06 +0700

    Update ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,7 +1,38 @@
 const ui = require("ui-lib/library");
 var uikick;
 
-ui.addMenuButton("Alex Kick", "admin", () => {
+const id = "#00000"
+const rawtime = 1 //in hours
+const reason = ""
+
+
+ui.addButton("Alex Kick", Icon.hammer, () => {
     uikick.show();
+})
+
+function kickRemote() {
+    const ID = id
+    const Time = rawtime
+    const Reason = reason
+
+    Call.sendChatMessage(`/kick ${ID} ${Time} ${Reason}`)
+}
+
+ui.onLoad(() => {
+    dialog = new BaseDialog("$kick-menu");
+    const table = dialog.cont;
+
+    table.label("Kick")
+    table.row()
+
+    ui.mobileAreaInput(table, "ID", id)
+    ui.mobileAreaInput(table, "Time", rawtime)
+    ui.mobileAreaInput(table, "Reason", reason)
+    table.row()
+
+    ui.addButton(table, "Kick", Icon.hammer, kickRemote)
+
+
+
 })
 
